e8ef9eff3333b4500fc9845f42aa0ae82f6bb8c9
Federico <federicopetrello2018@gmail.com>
2019-12-04 16:38:08 -0300

    ventas java

--- src/conexion/conect_db.java
+++ src/conexion/conect_db.java
@@ -1,26 +1,35 @@
 
 package conexion;
 import java.sql.*;
+import javax.swing.JOptionPane;
 public class conect_db {
     
     private static Connection cnx = null;
     
-     public static Connection obtener() throws SQLException, ClassNotFoundException {
-      if (cnx == null) {
-         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            cnx = DriverManager.getConnection("jdbc:mysql://localhost/sventas", "root", "foTiH8");
-         } catch (SQLException ex) {
-            throw new SQLException(ex);
-         } catch (ClassNotFoundException ex) {
-            throw new ClassCastException(ex.getMessage());
-         }
-      }
-      return cnx;
-   }
-   public static void cerrar() throws SQLException {
-      if (cnx != null) {
-         cnx.close();
-      }
-   }
+    String driver = "com.mysql.jdbc.Driver";
+	String url = "jdbc:mysql://localhost:3306/sventas?autoReconnect=true&useSSL=true";
+	String ip = "127.0.0.1";
+	String bd = "sventas";
+	String user = "root";
+	String password = "foTiH8";
+        
+       public conect_db()
+       {
+           	try {
+			Class.forName(driver).newInstance();
+			cnx = DriverManager.getConnection(url,user,password);
+		}catch(Exception e) {
+			JOptionPane.showMessageDialog(null, e.getMessage()+" \n Error en la Red.");
+		}
+       }
+       
+       public Connection getConexion() {
+		return cnx;
+	}
+	
+	public Connection cerrarConexion() throws SQLException{
+		cnx.close();
+		cnx = null;
+		return cnx;
+	}
 }
